c7726c2a48702acf9972aaf53498b4d3025b98dd
kandebonfim <kandebonfim@gmail.com>
2017-08-01 10:20:38 -0300

    Site updated to 2075780

--- assets/javascripts/application-fd0952f9.js
+++
@@ -1,1 +0,0 @@
-(function(){var t=[].indexOf||function(t){for(var n=0,e=this.length;e>n;n++)if(n in this&&this[n]===t)return n;return-1};t.call(document,"addEventListener")>=0&&document.addEventListener("DOMContentLoaded",function(){FastClick.attach(document.body)},!1)}).call(this),function(){var t;t=document.querySelectorAll("img.js-svg"),SVGInjector(t)}.call(this),function(){}.call(this);
